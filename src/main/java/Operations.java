ca8d337c51079d658a49824af3d10410f5e50145
Megan237 <megan.stinefield@bsu.edu>
2023-09-05 16:02:14 -0400

    square root completed with 2 tests

--- src/main/java/Operations.java
+++ src/main/java/Operations.java
@@ -3,4 +3,7 @@ public class Operations {
 
         return ;
     }
+    public double squareRoute(int number){
+        return Math.sqrt(number);
+    }
 }
--- src/test/java/operationsTest.java
+++ src/test/java/operationsTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class operationsTest {
     Operations operations = new Operations();
+
+    @Test
+    public void testSquareRoot() {
+        double result = operations.squareRoute(144);
+        Assertions.assertEquals(12, result);
+    }
+
+    @Test
+    public void testSquareRoot2() {
+        double result = operations.squareRoute(67);
+        Assertions.assertEquals(8.18535277187245, result);
+    }
 }
