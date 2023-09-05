a6523bcb7df1a38b3ac29ddaaf8505abd2d7e5f3
Brennan Hincks <bhhincks@bsu.edu>
2023-09-05 15:12:33 -0400

    accept user input

--- src/main/java/QuarternaryCalc.java
+++ src/main/java/QuarternaryCalc.java
@@ -4,6 +4,11 @@ public class QuarternaryCalc {
     public static void main(String[] args) throws Exception {
         DeciToQuart DQ = new DeciToQuart();
 
-        Scanner c = new Scanner(System.in);
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter 1st number");
+
+        String string = myObj.nextLine();
+        int result = DQ.converter(string);
+        System.out.println(result);
     }
 }
