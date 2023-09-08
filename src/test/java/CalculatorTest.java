69ce22bd94698af8c33d44ada0121cb94e4f2490
Brennan Hincks <bhhincks@bsu.edu>
2023-09-08 15:23:22 -0400

    Added functioning clear button

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
     public DeciToQuart calc = new DeciToQuart();
+    public QuartToDeci QDcalc = new QuartToDeci();
     @Test
     public void testQuaternary() {
         int result = calc.toQuart(57);
@@ -19,5 +20,13 @@ public class CalculatorTest {
         Assertions.assertEquals(13,QuartToDeci.quartToDeci(String.valueOf(31)));
         Assertions.assertEquals(28,QuartToDeci.quartToDeci(String.valueOf(130)));
         Assertions.assertEquals(46,QuartToDeci.quartToDeci(String.valueOf(232)));
+    }
+
+    @Test
+    public void testQuartToDeciToQuart(){
+        int Qresult = QDcalc.quartToDeci("321");
+        int Dresult = calc.toQuart(Qresult);
+        Assertions.assertEquals(57,Qresult);
+        Assertions.assertEquals(321,Dresult);
     }
 }
